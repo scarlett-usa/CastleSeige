a1c2d37d0d29088843e61d79f10f39ed2980f864
Matthew Cucco <mcucco@scarlettusa.com>
2019-11-10 16:18:43 -0500

    Added ProBuilder & ProGrids

---
+++ Assets/Scripts/BattleController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+    public int BOARD_X = 3;
+    public int BOARD_Y = 6;
+    public GameObject SpawnPrefab;
+
+
+    #region Private Properties
+    private const float _SPACER = 0.5f;
+    private List<GameObject> _Anchors { get; set; }
+    private GameObject SelectedTile {get; set;}
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _CreateAnchors();//Create Anchors
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if(hit.transform.tag == "Anchor"){
+                    Debug.Log("Selected Anchor!");
+                    if(SelectedTile != null) _MoveTile(hit.transform);
+                }
+                else if(hit.transform.tag == "Tile"){
+                    Debug.Log("Selected Tile!");
+                    if(SelectedTile == null){
+                        SelectedTile = hit.transform.gameObject;
+                    }
+                }
+            }
+        }
+    }
+
+    private void _CreateAnchors()
+    {
+        _Anchors = new List<GameObject>();
+        var anchorContainer = new GameObject("Anchors");
+        for (int x = 0; x < BOARD_X; x++)
+        {
+            for (int y = 0; y < BOARD_Y; y++)
+            {
+                var anchor = Instantiate(SpawnPrefab, new Vector3(x + _SPACER, 0f, y + _SPACER), Quaternion.identity);
+                anchor.name = $"Anchor-{x}-{y}";
+                _Anchors.Add(anchor);
+                anchor.transform.SetParent(anchorContainer.transform, true);
+            }
+        }
+    }
+
+    private void _MoveTile(Transform anchor){
+        SelectedTile.transform.Translate(anchor.position);
+        SelectedTile = null;
+    }
+}
---
+++ Assets/Scripts/ScoreSpinner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Attach to the spinner object. Rotates the spinner a certain amount
+public class ScoreSpinner : MonoBehaviour
+{
+    private Quaternion rotationStart;
+    public float rotationSpeed = 1.0f;
+    private bool reset = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationStart = transform.rotation; //saves initial rotation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(reset){
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationStart, Time.deltaTime * rotationSpeed);
+            if(Mathf.Round(Mathf.Abs(transform.rotation.z))  == Mathf.Round(Mathf.Abs(rotationStart.z)) )
+                reset = false;
+        }
+    }
+
+    //Rotates the spinner on it's z-axis
+    public void IncreaseScore(){
+        gameObject.transform.Rotate(new Vector3(0f, 0f, 60f), Space.Self);
+    }
+    public void ResetSpinner(){
+        reset = true;
+    }
+}
